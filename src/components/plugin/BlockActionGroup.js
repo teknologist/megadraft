69838ac69a0a5d6694aca17cf852568c7e32609f
Eric Taïeb Walch <eric@teknologism.com>
2017-11-09 17:13:05 +0100

    Bugfix for merge

--- src/components/plugin/BlockActionGroup.js
+++ src/components/plugin/BlockActionGroup.js
@@ -7,29 +7,25 @@
 import React, {Component} from "react";
 import PropTypes from "prop-types";
 
-import React, { Component } from "react";
-
 import BlockAction from "./BlockAction";
 
-
 export default class BlockActionGroup extends Component {
-
   static propTypes = {
     items: PropTypes.arrayOf(
       PropTypes.shape({
         key: PropTypes.string.isRequired,
         icon: PropTypes.func.isRequired,
-        action: PropTypes.func.isRequired
-      })
-    )
-  }
+        action: PropTypes.func.isRequired,
+      }),
+    ),
+  };
 
   renderItem(item) {
-    return(<BlockAction item={item} key={item.key} />);
+    return <BlockAction item={item} key={item.key} />;
   }
 
   render() {
-    return(
+    return (
       <ul className="block__action-group">
         {this.props.items.map(this.renderItem)}
       </ul>
